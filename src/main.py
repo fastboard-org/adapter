3deb295e3678b6438cfd4150214f9e20d6c11ebb
TGRodriguez <tgrodriguez@fi.uba.ar>
2024-07-18 01:30:49 -0300

    feat: Added AdapterRouter to FastAPI app

--- src/main.py
+++ src/main.py
@@ -1,12 +1,13 @@
 from fastapi import FastAPI
 import uvicorn
 from configs.settings import settings
-
+from routers.adapter import AdapterRouter
 from fastapi.exceptions import RequestValidationError
 from errors import CustomException, handle_validation_error, handle_custom_exception
 
 
 app = FastAPI()
+app.include_router(AdapterRouter)
 app.add_exception_handler(RequestValidationError, handle_validation_error)
 app.add_exception_handler(CustomException, handle_custom_exception)
 
