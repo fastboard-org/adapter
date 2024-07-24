e043b3e2458c30c25902dd4008a5ac8763842236
TGRodriguez <tgrodriguez@fi.uba.ar>
2024-07-24 17:50:44 -0300

    fix: Changed replace_parameters to support lists and dictionaries

--- src/lib/parameters.py
+++ src/lib/parameters.py
@@ -1,6 +1,22 @@
-def replace_parameters(string, parameters):
+def replace_parameters_in_string(string, parameters):
     for parameter, parameter_value in parameters.items():
         parameter_to_replace = "{{" + parameter + "}}"
         if parameter_to_replace in string:
-            string = string.replace(parameter_to_replace, parameter_value)
+            string = string.replace(parameter_to_replace, str(parameter_value))
     return string
+
+
+def replace_parameters(obj, parameters):
+    if isinstance(obj, str):
+        return replace_parameters_in_string(obj, parameters)
+    elif isinstance(obj, list):
+        return [replace_parameters(item, parameters) for item in obj]
+    elif isinstance(obj, dict):
+        new_dict = {}
+        for key, value in obj.items():
+            new_key = replace_parameters(key, parameters)
+            new_value = replace_parameters(value, parameters)
+            new_dict[new_key] = new_value
+        return new_dict
+    else:
+        return obj
