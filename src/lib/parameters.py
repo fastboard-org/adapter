52b185b3db21db6804e8d5b6e9ae5c3720a824bf
TGRodriguez <tgrodriguez@fi.uba.ar>
2024-08-17 19:35:57 -0300

    fix: Fixed error with amount of parameters

--- src/lib/parameters.py
+++ src/lib/parameters.py
@@ -1,29 +1,30 @@
 import ast
 
 
-def replace_parameters_in_string(string, parameters):
+def replace_parameters_in_string(string, parameters, used_parameters):
     for parameter, parameter_value in parameters.items():
         parameter_to_replace = "{{" + parameter + "}}"
         if parameter_to_replace in string:
             string = string.replace(parameter_to_replace, str(parameter_value))
+            used_parameters.add(parameter)
     return string
 
 
-def replace_parameters(obj, parameters):
+def replace_parameters(obj, parameters, used_parameters=set()):
     if isinstance(obj, str):
-        replaced_string = replace_parameters_in_string(obj, parameters)
+        replaced_string = replace_parameters_in_string(obj, parameters, used_parameters)
         try:
             eval = ast.literal_eval(replaced_string)
             return eval
         except (ValueError, SyntaxError):
             return replaced_string
     elif isinstance(obj, list):
-        return [replace_parameters(item, parameters) for item in obj]
+        return [replace_parameters(item, parameters, used_parameters) for item in obj]
     elif isinstance(obj, dict):
         new_dict = {}
         for key, value in obj.items():
-            new_key = replace_parameters(key, parameters)
-            new_value = replace_parameters(value, parameters)
+            new_key = replace_parameters(key, parameters, used_parameters)
+            new_value = replace_parameters(value, parameters, used_parameters)
             new_dict[new_key] = new_value
         return new_dict
     else:
--- src/repositories/query.py
+++ src/repositories/query.py
@@ -1,7 +1,8 @@
 from request import make_request
 from models.query import Query
 from lib.parameters import replace_parameters
-from errors import CustomException, ERR_UNSUPPORTED_QUERY_TYPE
+from errors import CustomException, ERR_UNSUPPORTED_QUERY_TYPE, ERR_MISSING_PARAMETERS
+import re
 
 
 class QueryRepository:
@@ -35,26 +36,84 @@ class QueryRepository:
             description=f"Unsupported query type: {query.type}",
         )
 
+    # async def execute_api_query(self, query: Query):
+    #     parameters = {**query.variables, **query.parameters}
+    #     # Fill header parameters
+    #     headers = {}
+    #     for key, value in query.headers.items():
+    #         replaced_key = replace_parameters(key, parameters)
+    #         replaced_value = replace_parameters(value, parameters)
+    #         headers[replaced_key] = replaced_value
+
+    #     # Fill body parameters
+    #     body = {}
+    #     for key, value in query.body.items():
+    #         replaced_key = replace_parameters(key, parameters)
+    #         replaced_value = replace_parameters(value, parameters)
+    #         body[replaced_key] = replaced_value
+
+    #     # Fill path parameters
+    #     path = replace_parameters(query.path, parameters)
+
+    #     url = query.credentials["main_url"] + path
+
+    #     response = await make_request(
+    #         url=url, headers=headers, method=query.method, body=body
+    #     )
+    #     return response
+
     async def execute_api_query(self, query: Query):
         parameters = {**query.variables, **query.parameters}
+        used_parameters = set()
+
         # Fill header parameters
         headers = {}
         for key, value in query.headers.items():
-            replaced_key = replace_parameters(key, parameters)
-            replaced_value = replace_parameters(value, parameters)
+            replaced_key = replace_parameters(key, parameters, used_parameters)
+            replaced_value = replace_parameters(value, parameters, used_parameters)
             headers[replaced_key] = replaced_value
 
         # Fill body parameters
         body = {}
         for key, value in query.body.items():
-            replaced_key = replace_parameters(key, parameters)
-            replaced_value = replace_parameters(value, parameters)
+            replaced_key = replace_parameters(key, parameters, used_parameters)
+            replaced_value = replace_parameters(value, parameters, used_parameters)
             body[replaced_key] = replaced_value
 
         # Fill path parameters
-        path = replace_parameters(query.path, parameters)
+        path = replace_parameters(query.path, parameters, used_parameters)
 
         url = query.credentials["main_url"] + path
+
+        # Validate that all parameters were used
+        unused_parameters = set(parameters.keys()) - used_parameters
+        if unused_parameters:
+            raise CustomException(
+                status_code=400,
+                error_code=ERR_MISSING_PARAMETERS,
+                description=f"Unused parameters in input: {unused_parameters}",
+            )
+
+        # Validate that all placeholders were replaced
+        all_placeholders = re.findall(r"\{\{(.*?)\}\}", path)
+        all_placeholders += [
+            match
+            for k, v in headers.items()
+            for match in re.findall(r"\{\{(.*?)\}\}", k + v)
+        ]
+        all_placeholders += [
+            match
+            for k, v in body.items()
+            for match in re.findall(r"\{\{(.*?)\}\}", k + str(v))
+        ]
+
+        missing_parameters = set(all_placeholders) - set(parameters.keys())
+        if missing_parameters:
+            raise CustomException(
+                status_code=400,
+                error_code=ERR_MISSING_PARAMETERS,
+                description=f"Missing parameters in input: {missing_parameters}",
+            )
 
         response = await make_request(
             url=url, headers=headers, method=query.method, body=body
