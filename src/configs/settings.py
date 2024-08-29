684e075512f278a789e579555e3db0b86bf82848
unknown <tgrodriguez@fi.uba.ar>
2024-08-29 12:21:19 -0300

    update: Added API_KEY secret and changed all env vars to uppercase

--- src/configs/settings.py
+++ src/configs/settings.py
@@ -2,9 +2,10 @@ from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-    app_host: str
-    app_port: int
-    dashboards_service_url: str
+    APP_HOST: str
+    APP_PORT: int
+    DASHBOARDS_SERVICE_URL: str
+    API_KEY: str
 
     class Config:
         env_file = ".env"
