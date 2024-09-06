30d3c7aa648db52b5ccfdc844f09709196852401
unknown <tgrodriguez@fi.uba.ar>
2024-09-06 00:44:58 -0300

    feat: Added MongoMetadata and ApiMetadata schemas for preview requests

--- src/schemas/rest_api.py
+++ src/schemas/rest_api.py
@@ -1,13 +1,25 @@
 from pydantic import BaseModel
+from typing import Union
 
 
 class ExecuteQueryRequest(BaseModel):
     parameters: dict
 
 
-class PreviewQueryRequest(BaseModel):
-    parameters: dict
+class ApiMetadata(BaseModel):
     method: str
     path: str
     headers: dict
     body: dict
+
+
+class MongoMetadata(BaseModel):
+    method: str
+    collection: str
+    filter_body: dict
+    update_body: dict
+
+
+class PreviewQueryRequest(BaseModel):
+    parameters: dict
+    connection_metadata: Union[ApiMetadata, MongoMetadata]
