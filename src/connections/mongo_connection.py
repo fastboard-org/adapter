ec08b2bfe98ef7eb2f51ed459486df8cc7d4a604
unknown <tgrodriguez@fi.uba.ar>
2024-09-06 00:46:21 -0300

    update: Parse object id values

--- src/connections/mongo_connection.py
+++ src/connections/mongo_connection.py
@@ -1,5 +1,4 @@
 import pymongo
-from bson.code import Code
 from bson.objectid import ObjectId
 
 
@@ -15,11 +14,8 @@ async def execute_query(
     switcher = {
         "aggregate": db.aggregate,
         "count": db.count_documents,
-        "deleteOne": db.delete_one,
-        "deleteMany": db.delete_many,
         "distinct": db.distinct,
         "find": db.find,
-        # "findAndModify": db.__find_and_modify,
         "findOne": db.find_one,
         "findOneAndDelete": db.find_one_and_delete,
         "findOneAndReplace": db.find_one_and_replace,
@@ -28,32 +24,15 @@ async def execute_query(
         "insertMany": db.insert_many,
         "updateOne": db.update_one,
         "updateMany": db.update_many,
+        "deleteOne": db.delete_one,
+        "deleteMany": db.delete_many,
     }
+    print(
+        f"Executing {method} on {collection} with filter {filter_body} and update {update_body}"
+    )
     res = switcher.get(method)(filter_body, update_body)
-    print(res, type(res))
-    # change all objectIDs on res to strings
-    for k, v in res.items():
-        if isinstance(v, ObjectId):
-            res[k] = str(v)
+    if res:
+        for k, v in res.items():
+            if isinstance(v, ObjectId):
+                res[k] = str(v)
     return res
-
-
-"""
-export enum MONGO_METHOD {
-  AGGREGATE = "aggregate",
-  COUNT = "count",
-  DELETE_ONE = "deleteOne",
-  DELETE_MANY = "deleteMany",
-  DISTINCT = "distinct",
-  FIND = "find",
-  FIND_AND_MODIFY = "findAndModify",
-  FIND_ONE = "findOne",
-  FIND_ONE_AND_DELETE = "findOneAndDelete",
-  FIND_ONE_AND_REPLACE = "findOneAndReplace",
-  FIND_ONE_AND_UPDATE = "findOneAndUpdate",
-  INSERT_ONE = "insertOne",
-  INSERT_MANY = "insertMany",
-  UPDATE_ONE = "updateOne",
-  UPDATE_MANY = "updateMany",
-}
-"""
