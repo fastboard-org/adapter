0c543681b03b259287cd38d42be0dc3127c932ff
unknown <tgrodriguez@fi.uba.ar>
2024-09-07 14:52:56 -0300

    feat: Added Custom LRU Cache for connections and changed switch to if-else statements because of the different ways of handling each method

--- src/connections/mongo_connection.py
+++ src/connections/mongo_connection.py
@@ -2,35 +2,105 @@ import pymongo
 from bson.objectid import ObjectId
 
 
+from cachetools import TLRUCache
+import time
+
+INACTIVITY_TIMEOUT = 60 * 5
+
+
+class CustomCache(TLRUCache):
+    def __delitem__(self, key):
+        print(f"Removing {key} from cache")
+        super().__delitem__(key)
+
+    def expire(self, time=None):
+        expired_items = list(super().expire(time))
+        for key, _ in expired_items:
+            print(f"Expiring {key} from cache")
+        return expired_items
+
+
+def custom_ttu(_key, _value, now):
+    return now + INACTIVITY_TIMEOUT
+
+
+cache = CustomCache(maxsize=20, ttu=custom_ttu, timer=time.monotonic)
+
+
+def get_mongo_client(connection_string: str):
+    if connection_string in cache:
+        print(f"Cache hit for {connection_string}")
+        return cache[connection_string]
+    print(f"Cache miss for {connection_string}")
+    client = pymongo.MongoClient(connection_string)
+    cache[connection_string] = client
+    return client
+
+
 async def execute_query(
     connection_string: str,
     collection: str,
     method: str,
-    filter_body: dict,
-    update_body: dict,
+    filter_body: dict = None,
+    update_body: dict = None,
 ):
-    client = pymongo.MongoClient(connection_string)
-    db = client.get_database()[collection]
-    switcher = {
-        "aggregate": db.aggregate,
-        "count": db.count_documents,
-        "distinct": db.distinct,
-        "find": db.find,
-        "findOne": db.find_one,
-        "findOneAndDelete": db.find_one_and_delete,
-        "findOneAndReplace": db.find_one_and_replace,
-        "findOneAndUpdate": db.find_one_and_update,
-        "insertOne": db.insert_one,
-        "insertMany": db.insert_many,
-        "updateOne": db.update_one,
-        "updateMany": db.update_many,
-        "deleteOne": db.delete_one,
-        "deleteMany": db.delete_many,
-    }
-    print(f"{method} on {collection}\n Filter: {filter_body}\n Update: {update_body}")
-    res = switcher.get(method)(filter_body, update_body)
-    if res:
+    db = get_mongo_client(connection_string).get_database()[collection]
+
+    if method == "aggregate":
+        res = list(db.aggregate(pipeline=[filter_body]))
+    elif method == "count":
+        res = db.count_documents(filter_body)
+    elif method == "distinct":
+        res = db.distinct(filter_body)
+    elif method == "find":
+        res = list(db.find(filter_body))
+    elif method == "findOne":
+        res = db.find_one(filter_body)
+    elif method == "findOneAndDelete":
+        res = db.find_one_and_delete(filter_body)
+    elif method == "findOneAndReplace":
+        res = db.find_one_and_replace(filter_body, update_body)
+    elif method == "findOneAndUpdate":
+        res = db.find_one_and_update(filter_body, update_body)
+    elif method == "insertOne":
+        res = db.insert_one(filter_body).inserted_id
+    elif method == "insertMany":
+        res = list(db.insert_many(filter_body))
+    elif method == "updateOne":
+        modified_count = db.update_one(filter_body, update_body).modified_count
+        if modified_count == 0:
+            res = {}
+        else:
+            document = db.find_one(filter_body)
+            res = document
+    elif method == "updateMany":
+        modified_count = db.update_many(filter_body, update_body).modified_count
+        if modified_count == 0:
+            res = {}
+        else:
+            documents = list(db.find(filter_body))
+            res = documents
+    elif method == "deleteOne":
+        deleted_count = db.delete_one(filter_body).deleted_count
+        res = {"deleted_count": deleted_count}
+    elif method == "deleteMany":
+        deleted_count = db.delete_many(filter_body).deleted_count
+        res = {"deleted_count": deleted_count}
+
+    if isinstance(res, ObjectId):
+        res = str(res)
+
+    if isinstance(res, dict):
         for k, v in res.items():
             if isinstance(v, ObjectId):
                 res[k] = str(v)
+    elif isinstance(res, list):
+        for item in res:
+            if isinstance(item, ObjectId):
+                item = str(item)
+            if isinstance(item, dict):
+                for k, v in item.items():
+                    if isinstance(v, ObjectId):
+                        item[k] = str(v)
+
     return res
