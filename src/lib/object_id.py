87e481b34b8b79b7d7081e4f544f277afef3365e
unknown <tgrodriguez@fi.uba.ar>
2024-09-11 23:48:25 -0300

    feat: Addedd replace_objectid_strings method with its tests

---
+++ src/lib/object_id.py
@@ -0,0 +1,13 @@
+from bson import ObjectId
+import re
+
+def replace_objectid_strings(data):
+    if isinstance(data, dict):
+        return {replace_objectid_strings(key): replace_objectid_strings(value) for key, value in data.items()}
+    elif isinstance(data, list):
+        return [replace_objectid_strings(item) for item in data]
+    elif isinstance(data, str):
+        match = re.match(r"ObjectId\('([0-9a-fA-F]{24})'\)", data)
+        if match:
+            return ObjectId(match.group(1))
+    return data
---
+++ tests/lib/test_object_id.py
@@ -0,0 +1,78 @@
+from src.lib.object_id import replace_objectid_strings
+from bson import ObjectId
+
+
+def test_replace_empty_dict():
+    assert replace_objectid_strings({}) == {}
+
+
+def test_replace_objectid_strings_on_string():
+    string = "ObjectId('5f6c6f6e6e656374696f6f6e')"   
+    replaced_string = replace_objectid_strings(string)
+    assert replaced_string == ObjectId("5f6c6f6e6e656374696f6f6e")
+
+
+def test_replace_objectid_strings_on_list_of_strings():
+    list_of_strings = [
+        "ObjectId('5f6c6f6e6e656374696f6f6e')",
+        "ObjectId('5f6c6f6e6e656374696f6f6f')",
+    ]
+    replaced_list = replace_objectid_strings(list_of_strings)
+    assert replaced_list == [
+        ObjectId("5f6c6f6e6e656374696f6f6e"),
+        ObjectId("5f6c6f6e6e656374696f6f6f"),
+    ]
+
+
+def test_replace_objectid_strings_on_dict():
+    dictionary = {
+        "id": "ObjectId('5f6c6f6e6e656374696f6f6e')",
+        "name": "ditto",
+    }
+    replaced_dict = replace_objectid_strings(dictionary)
+    assert replaced_dict == {
+        "id": ObjectId("5f6c6f6e6e656374696f6f6e"),
+        "name": "ditto",
+    }
+
+def test_replace_objectid_strings_on_nested_dict():
+    dictionary = {
+        "id": "ObjectId('5f6c6f6e6e656374696f6f6e')",
+        "name": "ditto",
+        "nested": {
+            "id": "ObjectId('5f6c6f6e6e656374696f6f6f')",
+            "name": "eevee",
+        },
+    }
+    replaced_dict = replace_objectid_strings(dictionary)
+    assert replaced_dict == {
+        "id": ObjectId("5f6c6f6e6e656374696f6f6e"),
+        "name": "ditto",
+        "nested": {
+            "id": ObjectId("5f6c6f6e6e656374696f6f6f"),
+            "name": "eevee",
+        },
+    }
+
+def test_replace_objectid_string_on_nested_dict_with_objectid_as_key_and_value():
+    dictionary = {
+        "id": "ObjectId('5f6c6f6e6e656374696f6f6e')",
+        "name": "ditto",
+        "nested": {
+            "ObjectId('5f6c6f6e6e656374696f6f6f')": "ObjectId('5f6c6f6e6e656374696f6f6e')",
+            "name": "eevee",
+        },
+        "ObjectId('5f6c6f6e6e656374696f6f6f')": ["ObjectId('5f6c6f6e6e656374696f6f6e')"],
+    }
+    replaced_dict = replace_objectid_strings(dictionary)
+    assert replaced_dict == {
+        "id": ObjectId("5f6c6f6e6e656374696f6f6e"),
+        "name": "ditto",
+        "nested": {
+            ObjectId("5f6c6f6e6e656374696f6f6f"): ObjectId("5f6c6f6e6e656374696f6f6e"),
+            "name": "eevee",
+        },
+        ObjectId("5f6c6f6e6e656374696f6f6f"): [ObjectId("5f6c6f6e6e656374696f6f6e")],
+    }
+
+
