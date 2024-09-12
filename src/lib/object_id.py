f80bfd7446b4242fff33dcc7429c654e368ad080
unknown <tgrodriguez@fi.uba.ar>
2024-09-11 23:52:55 -0300

    update: Linter and formatter

--- src/lib/object_id.py
+++ src/lib/object_id.py
@@ -1,9 +1,13 @@
 from bson import ObjectId
 import re
 
+
 def replace_objectid_strings(data):
     if isinstance(data, dict):
-        return {replace_objectid_strings(key): replace_objectid_strings(value) for key, value in data.items()}
+        return {
+            replace_objectid_strings(key): replace_objectid_strings(value)
+            for key, value in data.items()
+        }
     elif isinstance(data, list):
         return [replace_objectid_strings(item) for item in data]
     elif isinstance(data, str):
